3d2ea21db1773f258da78508a216da221a6a4491
Aziz Sattorov <108568027+thtflx@users.noreply.github.com>
2023-07-17 20:54:18 +0300

    deploy starter pr

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function Context() {
+  return (
+    <div className="container bg-green-200 px-2 py-4 ">
+      <h3>Title</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, id.</p>
+    </div>
+  )
+}
+
+
+export default function App() {
+  return (
+    <div className="container">
+      <Context />
+      Some app.
+    </div>
+  )
+}
